ccdedf1fdd8c60fbcd940a1c4278b5d2d09ed22e
Rick Mansfield <PEACEFULRICK@msn.com>
2022-03-27 17:11:23 -0500

    Add project files.

---
+++ Operators/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int answer = 7 + 3 * 4;
+            //Console.WriteLine(answer);
+
+            //double radius = 12;
+            //double area = Math.PI * radius * radius;
+
+            int x = 3;
+            int y = x++;
+            Console.WriteLine($"Using x++, x is {x} and y is {y}");
+        }
+    }
+}
