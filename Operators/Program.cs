fb22d75e5b1789fcb5e5edc96b75afc124637894
Rick Mansfield <PEACEFULRICK@msn.com>
2022-05-09 17:32:21 -0500

    Section 8 118 Using Remainder Operator

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -6,37 +6,53 @@ namespace Operators
     {
         static void Main(string[] args)
         {
-            int x = 3;
-            //int y = x++;
-            int y = x;
-            x++;
-            Console.WriteLine($"Using x++, x is {x} and y is {y}");
-            // Using x++, x is 4 and y is 3
+            //int x = 3;
+            ////int y = x++;
+            //int y = x;
+            //x++;
+            //Console.WriteLine($"Using x++, x is {x} and y is {y}");
+            //// Using x++, x is 4 and y is 3
 
-            x = 3;
-            //y = ++x;
-            x++;
-            y = x;
-            Console.WriteLine($"Using ++x, x is {x} and y is {y}");
-            // Using ++x, x is 4 and y is 4
+            //x = 3;
+            ////y = ++x;
+            //x++;
+            //y = x;
+            //Console.WriteLine($"Using ++x, x is {x} and y is {y}");
+            //// Using ++x, x is 4 and y is 4
 
-            Console.WriteLine("Don't do this!");
-            x = 3;
-            //y = ++x - x++;
-            x++;
-            int z = x;
-            y = z - x;
-            x++;
-            Console.WriteLine($"++x - x++ gives: x is {x} and y is {y}");
-            // ++x - x++ gives: x is 5 and y is 0
+            //Console.WriteLine("Don't do this!");
+            //x = 3;
+            ////y = ++x - x++;
+            //x++;
+            //int z = x;
+            //y = z - x;
+            //x++;
+            //Console.WriteLine($"++x - x++ gives: x is {x} and y is {y}");
+            //// ++x - x++ gives: x is 5 and y is 0
 
-            x = 3;
-            //y = x++ - ++x;
-            z = x;
-            x += 2;
-            y = z - x;
-            Console.WriteLine($"x++ - ++x gives: x is {x} and y is {y}");
-            // x++ - ++x gives: x is 5 and y is -2
+            //x = 3;
+            ////y = x++ - ++x;
+            //z = x;
+            //x += 2;
+            //y = z - x;
+            //Console.WriteLine($"x++ - ++x gives: x is {x} and y is {y}");
+            //// x++ - ++x gives: x is 5 and y is -2
+            ///
+            //int x = 12; 
+            //int y = 4;
+            //if (x % y == 0)
+            //{
+            //    Console.WriteLine($"{y} divides exactly into {x}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{y} does not divide exactly into {x}");
+            //}
+            for (int month = 1; month <= 12; month++)
+            {
+                Console.WriteLine($"The next month after {month} is {month % 12 + 1}");
+            }
+            
         }
     }
 }
